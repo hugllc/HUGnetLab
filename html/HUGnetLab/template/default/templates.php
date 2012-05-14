c1dccc10c71ebac41abb593f59d06892746377b8
Scott Price <prices@hugllc.com>
2012-05-14 17:08:39 -0500

    Small tweaks on the template for bug #5765.

--- html/HUGnetLab/template/default/templates.php
+++ html/HUGnetLab/template/default/templates.php
@@ -13,16 +13,22 @@
             <td>{{FWPartNum}} {{FWVersion}}</td>
 </script>
 <script type="text/template" id="DevicePropertiesTemplate">
+    <div>
+        <button class="cancel" style="float: right; margin: 10px;">Back to List</button>
         <table>
-            <tr><th>Serial #</th><td>{{id}}</td></tr>
-            <tr><th>Device ID</th><td>{{DeviceID}}</td></tr>
-            <tr><th>Name</th><td>{{DeviceName}}</td></tr>
-            <tr><th>Hardware</th><td>{{HWPartNum}}</td></tr>
-            <tr><th>Firmware</th><td>{{FWPartNum}}</td></tr>
-            <tr><th>Version</th><td>{{FWVersion}}</td></tr>
-            <tr><th>Raw Setup</th><td>{{RawSetup}}</td></tr>
-            <tr><th>Action</th><td><button class="cancel">Back to List</button></td></tr>
+            <tr class="row0"><th>Serial #</th><td>{{id}}</td></tr>
+            <tr class="row1"><th>Device ID</th><td>{{DeviceID}}</td></tr>
+            <tr class="row0"><th>Name</th><td>{{DeviceName}}</td></tr>
+            <tr class="row1"><th>Hardware</th><td>{{HWPartNum}}</td></tr>
+            <tr class="row0"><th>Firmware</th><td>{{FWPartNum}}</td></tr>
+            <tr class="row1"><th>Version</th><td>{{FWVersion}}</td></tr>
+            <tr class="row0"><th>Raw Setup</th><td>{{RawSetup}}</td></tr>
+            {{#params}}
+            <tr><th colspan="2">Properties</th></tr>
+            <tr class="row0"><th>LastContact</th><td>{{LastContact}}</td></tr>
+            {{/params}}
         </table>
+    </div>
 </script>
 <script type="text/template" id="DeviceListTemplate">
         <table id="devTable" class="tablesorter">
