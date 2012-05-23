80b9d01cb0147308935c02791a68c4c672d3f276
Scott Price <prices@hugllc.com>
2012-05-23 15:38:52 -0500

    More tweaks for bug #5901.  I added some things to the displays.

--- html/HUGnetLab/template/default/templates.data.php
+++ html/HUGnetLab/template/default/templates.data.php
@@ -21,10 +21,17 @@
         <h2>View Test <%= id %></h2>
         <div>
             <div>
-                <div><label for="autorefresh" class="bold">Refresh:</label><input id="autorefresh" type="checkbox" class="autorefresh" value="5" /></div>
-                <div><label for="datarows" class="bold">Rows:</label><input id="datarows" type="input" class="datarows" value="<%= limit %>" /></div>
+                <div><label for="autorefresh" class="bold">Update with new data:</label><input id="autorefresh" type="checkbox" class="autorefresh" value="5" /></div>
+                <div>
+                    <label for="since" class="bold">From </label><input id="since" type="input" class="since" value="<%= since %>" />
+                    <label for="until" class="bold">To </label><input id="until" type="input" class="until" value="<%= until %>" />
+                    <input type="submit" name="submit" value="Go" />
+                </div>
             </div>
         </div>
         </form>
+        <div>
+            <span class="bold">Records:</span> <span id="data-records">0</span>
+        </div>
 </script>
 
