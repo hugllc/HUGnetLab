47c7d72427ac0254ed295db8114826fe06a41ce4
Scott Price <prices@hugllc.com>
2012-06-07 16:12:08 -0500

    Removed some now unused code for bug #5985.

--- html/HUGnetLab/template/default/templates.data.php
+++ html/HUGnetLab/template/default/templates.data.php
@@ -9,13 +9,6 @@
             <td class="<%= fieldClass %>"><%= data %></td>
 </script>
 
-<script type="text/template" id="DataPollTemplate">
-        <form id="pollForm" method="POST" action="javascript:void(0);">
-        <h2>Run Test <%= id %></h2>
-        <button class="startPoll">Start Polling</button>
-        <button class="stopPoll" style="display: none;">Stop Polling</button>
-        </form>
-</script>
 <script type="text/template" id="DataViewTemplate">
         <form id="pollForm" method="POST" action="javascript:void(0);">
         <h2>View Test <%= DeviceName %></h2>
