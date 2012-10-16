5adfe620035726db009ee24acdd204137ab0d59a
Scott Price <prices@hugllc.com>
2012-10-16 14:59:29 -0500

    A tweak for bug #6573.

--- deb/system/etc/hugnet/hugnet_remote_help.sh
+++ deb/system/etc/hugnet/hugnet_remote_help.sh
@@ -7,19 +7,10 @@ USER=`hostname`
 IP=sync.hugnetlab.com
 
 echo -e "Instructions:\n"
-echo -e "This script will allow" $MAINTAINER "to connect to your computer and bypass \nyour firewall. You must leave this running while he is connected. \nAfterwards, you MUST also close this window to make sure the connection \nis closed.\n======\n"
+echo -e "This script will allow" $MAINTAINER "to connect to your computer and bypass \nyour firewall. You must leave this running while they are connected. \nAfterwards, you MUST also close this window to make sure the connection \nis closed.\n======\n"
 
-#echo -ne "Call" $MAINTAINER "and ask for his IP Address. Type it in now. When finished, hit [Enter] \nIP= "
-
-#read IP
-
-#echo -n "Is this correct? (Y/n) "
-
-#read CHECK
-
-#if [ $CHECK == "y" -o $CHECK == "Y" ];
-#then
-sudo ssh -R 60022:localhost:22 -l $USER -p $PORT -N $IP	
+echo -e "Running SSH Client.  The script will appear to hang.  This is normal."
+sudo ssh -R 40022:localhost:22 -R 45902:localhost:5900 -R 40080:localhost:80 -l $USER -p $PORT -N $IP
 #else 
 #	echo "exiting."
 #	exit 1;
