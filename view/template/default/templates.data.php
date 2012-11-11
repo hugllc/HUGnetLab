4a94581fbc89c71998c1d20af0281af0d446bcac
Scott Price <prices@hugllc.com>
2012-11-10 21:28:55 -0600

    Fixed bug #6889.  I added the buttons.

--- view/template/default/templates.data.php
+++ view/template/default/templates.data.php
@@ -17,6 +17,11 @@
                 <div><button class="exportCSV">Export as CSV</button></div>
                 <div><label for="autorefresh" class="bold">Update with new data:</label><input id="autorefresh" type="checkbox" class="autorefresh" value="5" /></div>
                 <div>
+                    <button class="minute30">30 Minutes</button>
+                    <button class="minute240">4 Hours</button>
+                    <button class="minute720">12 Hours</button>
+                </div>
+                <div>
                     <label for="since" class="bold">From </label><input id="since" type="text" class="since" value="<%= sinceDate %>" />
                     <label for="until" class="bold">To </label><input id="until" type="text" class="until" value="<%= untilDate %>" />
                     <input type="submit" name="submit" value="Go" />
