51eb7b80a173614027327f00fe0b15d785195c58
Scott Price <prices@hugllc.com>
2013-06-18 09:00:20 -0500

    This changes the LastContact date to the LastHistoryPush.  This fixes #7879.

--- html/HUGnetLab/template/default/templates.devicelist.php
+++ html/HUGnetLab/template/default/templates.devicelist.php
@@ -31,7 +31,7 @@
                     <th class="sorter-text">Gateway</th>
                     <th class="sorter-text">Active</th>
                     <th class="sorter-numeric">Data Int<br />(Sec)</th>
-                    <th class="sorter-usLongDate">Last Contact</th>
+                    <th class="sorter-usLongDate">Last History</th>
                     <th class="sorter-usLongDate">Last Poll</th>
                 </tr>
                 </thead>
@@ -53,7 +53,7 @@
                         <td class="center"><% (Active == 1) ? print('Yes') : print('No'); %></td>
                         <td class="center"><%= PollInterval %></td>
                         <td>
-                            <%= formatDate(params.LastContact) %>
+                            <%= formatDate(params.LastHistoryPush) %>
                         </td>
                         <td>
                             <span class="<% (LatePoll) ? print('error') : print(''); %>">
