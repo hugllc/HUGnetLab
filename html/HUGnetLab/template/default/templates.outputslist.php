33a6c6534e47f6900eca9e17084b08d3bffaa2d7
Scott Price <prices@hugllc.com>
2013-10-20 14:41:53 -0500

    More work on bug #8044.  It is almost done.

--- html/HUGnetLab/template/default/templates.outputslist.php
+++ html/HUGnetLab/template/default/templates.outputslist.php
@@ -36,3 +36,56 @@
                             </span>
                         </td>
         </script>
+<script type="text/template" id="OutputsViewTemplate">
+        <form id="outputsForm" method="POST" action="javascript:void(0);">
+        </form>
+        <div>
+          <h3>Last Data Record:</h3>
+          <div id="OutputsViewData">
+          </div>
+        </div>
+        <div>
+          <h3>Control Channels:</h3>
+          <div id="OutputsControlChannelsDiv">
+          </div>
+        </div>
+</script>
+<script type="text/template" id="OutputsViewDataTemplate">
+          <table>
+            <tr>
+              <th>Date</th>
+          <%
+            for (var key in dataChannels) {
+                print(
+                    '<th>'+dataChannels[key].label+' ('+dataChannels[key].units+')</th>'
+                );
+            }
+          %>
+            </tr>
+            <tr>
+              <td><%= sqlUTCDate(params.LastPollData.Date * 1000) %></td>
+          <%
+            for (var key in dataChannels) {
+                print('<td align="center">'+params.LastPollData[key].value+'</td>');
+            }
+          %>
+            </tr>
+          </table>
+</script>
+<script type="text/template" id="OutputsControlChannelEntryTemplate">
+              <th><%= label %></th>
+              <td align="center"><%= value %></td>
+              <td><input type="text" name="value" value="<% print(value + 0); %>" /></td>
+</script>
+<script type="text/template" id="OutputsControlChannelListTemplate">
+          <table>
+            <thead>
+              <tr>
+                <th>Channel</th><th>Current Value</th><th>New Value</th>
+              </tr>
+            </thead>
+            <tbody>
+            </tbody>
+          </table>
+
+</script>
