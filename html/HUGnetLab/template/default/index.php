f533454f21f13405e4c90b1a3febdcf91d1a38c3
Scott Price <prices@hugllc.com>
2014-05-27 09:36:21 -0500

    This fixes #8235.  I added version 1.4.5.

--- html/HUGnetLab/template/default/index.php
+++ html/HUGnetLab/template/default/index.php
@@ -8,6 +8,7 @@
         <link rel="stylesheet" href="HUGnetLab/template/default/default.css" />
         <link rel="stylesheet" href="HUGnetLab/template/default/theme.default.css" />
         <link rel="stylesheet" href="HUGnetLab/template/default/jquery.jqplot.css" />
+        <link rel="stylesheet" href="HUGnetLab/template/default/jquery.svg.css" />
         <link rel="stylesheet" href="HUGnetLab/template/default/pepper-grinder/jquery-ui.css" />
         <script src="/HUGnetLib/contrib.js" type="text/javascript"></script>
         <script src="/HUGnetLib/hugnet.js" type="text/javascript"></script>
